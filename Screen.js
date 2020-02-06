682bea814b141c95de4658d76886de3e4c207adc
iipeter01430 <60205451+iipeter01430@users.noreply.github.com>
2020-02-05 17:42:55 -0700

    Create Screen.js

---
+++ Screen.js
@@ -0,0 +1,16 @@
+let workMode = true
+input.onButtonPressed(Button.A, function () {
+    workMode = false
+})
+input.onButtonPressed(Button.B, function () {
+    workMode = true
+})
+basic.forever(function () {
+    if (workMode) {
+  
+
+    }
+    else {
+
+}
+})

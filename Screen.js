8b09b92216c0c35261cc36ccdd12870b1b3ae034
iipeter01430 <60205451+iipeter01430@users.noreply.github.com>
2020-02-05 19:51:24 -0700

    Update Screen.js

--- Screen.js
+++ Screen.js
@@ -14,5 +14,32 @@ basic.forever(function () {
     } else {
 
         basic.showString("Sleep!")
+        let ball = game.createSprite(2, 2);
+input.onButtonPressed(Button.B, function () {
+    ball.setX(Math.randomRange(0, 4))
+    ball.setY(Math.randomRange(0, 4))
+})
+
+input.onButtonPressed(Button.A, function () {
+    ball.turn(Direction.Left, 45)
+})
+basic.forever(function () {
+    ball.ifOnEdgeBounce();
+    ball.move(1);
+})
+
     }
+})
+let ball = game.createSprite(2, 2);
+input.onButtonPressed(Button.B, function () {
+    ball.setX(Math.randomRange(0, 4))
+    ball.setY(Math.randomRange(0, 4))
+})
+
+input.onButtonPressed(Button.A, function () {
+    ball.turn(Direction.Left, 45)
+})
+basic.forever(function () {
+    ball.ifOnEdgeBounce();
+    ball.move(1);
 })
